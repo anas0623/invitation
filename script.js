f1e9fdea8e7b477cc26f826ac52be7e033dbedba
anas0623 <smohamedanas5@gmail.com>
2026-06-12 12:12:45 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+// Countdown Timer
+var weddingDate = new Date("Sep 06, 2026 12:15:00").getTime();
+
+var x = setInterval(function() {
+
+    var now = new Date().getTime();
+    var distance = weddingDate - now;
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("timer").innerHTML =
+    days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+}, 1000);
+
+// PDF Download Button
+function downloadPDF(){
+    window.print();
+}
